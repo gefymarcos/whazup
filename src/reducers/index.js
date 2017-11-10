c5e77d46fc8d387e97d38fd00c37811ffcdb9d36
Gefy Marcos <gefymarcos@gmail.com>
2017-11-10 18:10:10 -0200

    add new reducer listTalks

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,10 +4,12 @@ import auth from './auth';
 import app from './app';
 import contacts from './contacts';
 import talks from './talks';
+import listTalks from './listTalks';
 
 export default combineReducers({
   auth,
   app,
   contacts,
-  talks
+  talks,
+  listTalks
 });
