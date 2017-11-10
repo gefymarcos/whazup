853cedbf7ebcef268ddaa243451a02d6ad05924a
Gefy Marcos <gefymarcos@gmail.com>
2017-11-10 18:44:20 -0200

    finished talks screen

--- src/components/Talks.js
+++ src/components/Talks.js
@@ -1,8 +1,54 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { ListView } from 'react-native';
+import { connect } from 'react-redux';
+import _ from 'lodash';
+import { Actions } from 'react-native-router-flux';
+import { chatsUserFetch } from '../actions/app';
+import ListViewTalks from '../components/commons/ListViewTalks';
 
-export default props => (
-  <View style={{ flex: 1 }}>
-    <Text>Conversas</Text>
-  </View>
-);
+class Talks extends React.Component {
+  componentWillMount() {
+    this.props.chatsUserFetch();
+    this.createData(this.props.talks);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.createData(nextProps.talks);
+  }
+
+  createData(talks) {
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r2 !== r1 });
+    this.dataSource = ds.cloneWithRows(talks);
+  }
+
+  renderRow(talk) {
+    return (
+      <ListViewTalks
+        title={talk.nome}
+        onPress={() => Actions.messages({ title: talk.nome, contactName: talk.nome, contactEmail: talk.email })}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <ListView 
+        enableEmptySections
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
+    );
+  }
+}
+  
+const mapStateToProps = state => {
+  const talks = _.map(state.listTalks, (val, uid) => {
+    return { ...val, uid };
+  });
+
+  return {
+    talks
+  };
+};
+
+export default connect(mapStateToProps, { chatsUserFetch })(Talks);
---
+++ src/components/commons/ListViewTalks.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, StyleSheet, Text, TouchableHighlight } from 'react-native';
+
+export default props => (
+  <TouchableHighlight
+    onPress={() => props.onPress()}
+  >
+    <View style={styles.container}>
+      <Text style={styles.title}>{props.title}</Text>
+    </View>
+  </TouchableHighlight>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    borderBottomWidth: 1,
+    borderColor: '#CCC'
+  },
+  title: {
+    fontSize: 25,
+  },
+  subtitle: {
+    fontSize: 16,
+    marginLeft: 2,
+    color: '#444'
+  }
+});
